5c6d09f1a021c50f2c3686821c21ce632776e425
Alexis Tan <alexis@sen-dev.com>
2018-01-09 01:45:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+AvaIre Shared
+=============
+
+Shared is a small collection of share properties and values used by AvaIre and related projects, the properties lives in this project to ensure that all projects using these properties are always using the right values.
---
+++ src/main/java/com/avairebot/shared/ExitCodes.java
@@ -0,0 +1,9 @@
+package com.avairebot.shared;
+
+public class ExitCodes {
+
+    public static final int EXIT_CODE_NORMAL = 0;
+    public static final int EXIT_CODE_ERROR = 1;
+    public static final int EXIT_CODE_RESTART = 10;
+    public static final int EXIT_CODE_UPDATE = 11;
+}
