d0cfe4549d5ad3cdd6b95ba292878872d89f2b4c
Alexis Tan <alexis@sen-dev.com>
2018-02-02 00:45:02 +0100

    Add bot id constant

--- src/main/java/com/avairebot/shared/DiscordConstants.java
+++ src/main/java/com/avairebot/shared/DiscordConstants.java
@@ -2,5 +2,7 @@ package com.avairebot.shared;
 
 public class DiscordConstants {
 
+    public static final long AVAIRE_BOT_ID = 275270122082533378L;
+
     public static final long FEEDBACK_CHANNEL_ID = 324301839170273280L;
 }
