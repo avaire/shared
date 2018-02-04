e0ec78fd821eaac706e2aef79c36ac0c0af5a954
Alexis Tan <alexis@sen-dev.com>
2018-02-04 01:12:33 +0100

    Add default command prefix to discord constants

--- src/main/java/com/avairebot/shared/DiscordConstants.java
+++ src/main/java/com/avairebot/shared/DiscordConstants.java
@@ -2,7 +2,8 @@ package com.avairebot.shared;
 
 public class DiscordConstants {
 
-    public static final long AVAIRE_BOT_ID = 275270122082533378L;
+    public static final String DEFAULT_COMMAND_PREFIX = "!";
 
+    public static final long AVAIRE_BOT_ID = 275270122082533378L;
     public static final long FEEDBACK_CHANNEL_ID = 324301839170273280L;
 }
