6405bfa912946e0e9c25db1db2b2035b39b0e4fe
Alexis Tan <alexis@sen-dev.com>
2018-04-10 01:56:56 +0200

    Add activity log channel id

--- src/main/java/com/avairebot/shared/DiscordConstants.java
+++ src/main/java/com/avairebot/shared/DiscordConstants.java
@@ -6,4 +6,5 @@ public class DiscordConstants {
 
     public static final long AVAIRE_BOT_ID = 275270122082533378L;
     public static final long FEEDBACK_CHANNEL_ID = 324301839170273280L;
+    public static final long ACTIVITY_LOG_CHANNEL_ID = 284114819278438401L;
 }
