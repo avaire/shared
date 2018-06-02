74223ba2760ef18d8be8af4e2c4593eee972cb37
Alexis Tan <alexis@sen-dev.com>
2018-06-02 23:49:53 +0200

    Create sentry constants

---
+++ src/main/java/com/avairebot/shared/SentryConstants.java
@@ -0,0 +1,12 @@
+package com.avairebot.shared;
+
+public class SentryConstants {
+
+    public static final String SENTRY_APPENDER_NAME = "SENTRY";
+
+    public static final String SENTRY_MDC_TAG_GUILD = "guild";
+    public static final String SENTRY_MDC_TAG_CHANNEL = "channel";
+    public static final String SENTRY_MDC_TAG_AUTHOR = "author";
+    public static final String SENTRY_MDC_TAG_SHARD = "shard";
+    public static final String SENTRY_MDC_TAG_MESSAGE = "message";
+}
