c5c9b06aa9ef7659d2c435bbd047eaf6d0004c0e
Alexis Tan <alexis@sen-dev.com>
2018-06-05 22:34:32 +0200

    Add senither bot id to discord constants

--- src/main/java/com/avairebot/shared/DiscordConstants.java
+++ src/main/java/com/avairebot/shared/DiscordConstants.java
@@ -5,6 +5,7 @@ public class DiscordConstants {
     public static final String DEFAULT_COMMAND_PREFIX = "!";
 
     public static final long AVAIRE_BOT_ID = 275270122082533378L;
+    public static final long SENITHER_BOT_ID = 386094646628974594L;
     public static final long FEEDBACK_CHANNEL_ID = 324301839170273280L;
     public static final long ACTIVITY_LOG_CHANNEL_ID = 284114819278438401L;
 }
