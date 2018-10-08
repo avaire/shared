fa64285010913575837b7b3d674ec12bfe5a53c1
Alexis Tan <alexis@sen-dev.com>
2018-10-08 19:10:42 +0200

    Add bot admin exception role
    The bot admin exception role is role that allows the use of some bot admin restricted commands without the user actually being a bot admin through the config, which would give them access to everything.

--- src/main/java/com/avairebot/shared/DiscordConstants.java
+++ src/main/java/com/avairebot/shared/DiscordConstants.java
@@ -8,4 +8,5 @@ public class DiscordConstants {
     public static final long SENITHER_BOT_ID = 386094646628974594L;
     public static final long FEEDBACK_CHANNEL_ID = 324301839170273280L;
     public static final long ACTIVITY_LOG_CHANNEL_ID = 284114819278438401L;
+    public static final long BOT_ADMIN_EXCEPTION_ROLE = 403265196040126464L;
 }
