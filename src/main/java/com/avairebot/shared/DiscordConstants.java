80809049d2d2b85880836d8e8cf898574aff993b
Alexis Tan <alexis@sen-dev.com>
2019-04-23 21:33:35 +0200

    Add changelog channel ID

--- src/main/java/com/avairebot/shared/DiscordConstants.java
+++ src/main/java/com/avairebot/shared/DiscordConstants.java
@@ -8,6 +8,7 @@ public class DiscordConstants {
     public static final long AVAIRE_BOT_ID = 275270122082533378L;
     public static final long SENITHER_BOT_ID = 386094646628974594L;
     public static final long FEEDBACK_CHANNEL_ID = 324301839170273280L;
+    public static final long CHANGELOG_CHANNEL_ID = 570225187661021214L;
     public static final long ACTIVITY_LOG_CHANNEL_ID = 284114819278438401L;
     public static final long BOT_ADMIN_EXCEPTION_ROLE = 403265196040126464L;
 }
